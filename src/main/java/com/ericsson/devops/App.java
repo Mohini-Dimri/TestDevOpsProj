dbe0d70ed5d11bd8c5bd2ab78e95fda6708268bd
Mohini Dimri <mohini.dimri@gmail.com>
2018-01-16 15:27:05 +0530

    First Commit

---
+++ src/main/java/com/ericsson/devops/App.java
@@ -0,0 +1,13 @@
+package com.ericsson.devops.com.ericsson.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
